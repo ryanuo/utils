a5ab4fb25e83b7133ac66157f6c06a613e092960
ryanuo <1577791638@qq.com>
2025-05-15 16:42:53 +0800

    refactor(script): update logger import path
    - Change logger import path from '../src' to '../src/node'

--- script/readme.ts
+++ script/readme.ts
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'node:url'
 import { dirname } from 'node:path'
 import typedocSidebar from '../docs/api/typedoc-sidebar.json'
 import { transDocsJson } from '../docs/.vitepress/utils'
-import { logger } from '../src'
+import { logger } from '../src/node'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
